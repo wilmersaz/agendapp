d499e6b6d890b5a9ef7c66c11a632f7791443973
Jhon Alejandro Murillo Diaz <56074441+JhonSnakee@users.noreply.github.com>
2024-09-21 16:34:57 -0500

    feat: welcome module

--- assets/js/app-script.js
+++ assets/js/app-script.js
@@ -1,55 +1,76 @@
 
-$(function() {
+$(function () {
     "use strict";
-     
-	 
-//sidebar menu js
-$.sidebarMenu($('.sidebar-menu'));
 
-// === toggle-menu js
+    //sidebar menu js
+    $.sidebarMenu($('.sidebar-menu'));
 
-$(".toggle-menu").on("click", function(e) {
+    // === toggle-menu js
+
+    $(".toggle-menu").on("click", function (e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
-    });	   
-	
-	   
-// === sidebar menu activation js
+    });
 
-$(function() {
-        for (var i = window.location, o = $(".sidebar-menu a").filter(function() {
+    // === sidebar menu activation js
+    $(function () {
+        for (var i = window.location, o = $(".sidebar-menu a").filter(function () {
             return this.href == i;
         }).addClass("active").parent().addClass("active"); ;) {
             if (!o.is("li")) break;
             o = o.parent().addClass("in").parent().addClass("active");
         }
-    }), 	   
-	   
-/* Back To Top */
+    }),
 
-$(document).ready(function(){ 
-    $(window).on("scroll", function(){ 
-        if ($(this).scrollTop() > 300) { 
-            $('.back-to-top').fadeIn(); 
-        } else { 
-            $('.back-to-top').fadeOut(); 
-        } 
-    }); 
-    $('.back-to-top').on("click", function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
-});	   
-	   
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+        /* Back To Top */
+        $(document).ready(function () {
+            $(window).on("scroll", function () {
+                if ($(this).scrollTop() > 300) {
+                    $('.back-to-top').fadeIn();
+                } else {
+                    $('.back-to-top').fadeOut();
+                }
+            });
+            $('.back-to-top').on("click", function () {
+                $("html, body").animate({ scrollTop: 0 }, 600);
+                return false;
+            });
+        });
 
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
 
 
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    function loadComplements() {
+        const sidebar = document.querySelector('.sidebarComp');
+        const topbar = document.querySelector('.topbarComp');
+        const footer = document.querySelector('.footerComp');
+
+
+        fetch('/app/common/sidebar.html')
+            .then(response => response.text())
+            .then(data => {
+                sidebar.innerHTML = data;
+            });
+
+        fetch('/app/common/topbar.html')
+            .then(response => response.text())
+            .then(data => {
+                topbar.innerHTML = data;
+            });
+
+        fetch('/app/common/footer.html')
+            .then(response => response.text())
+            .then(data => {
+                footer.innerHTML = data;
+            });
+    }
+
+    window.onload = loadComplements;
 
 });
