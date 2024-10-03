b049a46dfe352e688db6eec230aa33d4114a3070
Jhon Alejandro Murillo Diaz <56074441+JhonSnakee@users.noreply.github.com>
2024-10-03 09:16:08 -0500

    up: organizacion menu, header, footer

---
+++ assets/js/layout.js
@@ -0,0 +1,23 @@
+function loadTemplate(url, elementId) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById(elementId).innerHTML = data;
+                resolve(); // Resolvemos la promesa aquí
+            })
+            .catch(error => {
+                console.error('Error loading template con id: ', elementId, ' | ', error);
+                reject(error); // Rechazamos la promesa en caso de error
+            });
+    });
+}
+
+// Cargar plantillas iniciales
+loadTemplate('/app/common/menu_topbar.html', 'header');
+loadTemplate('/app/common/footer.html', 'footer');
